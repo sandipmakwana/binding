e9ce613ba31ce5b7316a53fdf091776f30cff48b
sandipmakwana <sandy007.makwana@gmail.com>
2020-03-17 21:28:39 -0400

    odd even component created with the usage of binding properties ( @Input , @Output)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'binding';
+ oddNumbers : number[] = [];
+ evenNumbers : number[] = [];
+
+  onIntervalFired(firedNumber : number)
+  {
+    if(firedNumber % 2 === 0){
+      this.evenNumbers.push(firedNumber);
+    }else{
+      this.oddNumbers.push(firedNumber);
+    }
+  console.log(firedNumber);
+  }
 }
